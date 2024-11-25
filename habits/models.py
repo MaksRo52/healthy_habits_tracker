5dbc6a804e77a4c03e9f6b00f01604f175a0284c
m.romantsov <romancov_dzr@mail.ru>
2024-11-25 17:10:03 +0300

    create models and settings

---
+++ habits/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+NULLABLE = {"blank": True, "null": True}
+
+class Habit(models.Model):
+    pass
+
---
+++ users/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone_number = PhoneNumberField(
+        null=True, blank=True, verbose_name="Номер телефона"
+    )
+    country = models.CharField(
+        max_length=50, verbose_name="Страна", null=True, blank=True
+    )
+    photo = models.ImageField(
+        upload_to="users/photos/",
+        verbose_name="Фото",
+        help_text="Загрузите своё фото.",
+        null=True,
+        blank=True,
+    )
+    token = models.CharField(
+        max_length=100, verbose_name="Токен", null=True, blank=True
+    )
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
